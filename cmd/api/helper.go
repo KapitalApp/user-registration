0cd0b28d1fce670b4a7cf71854232af51cc083a7
Pouria Amini <aminipooria1@gmail.com>
2023-02-03 14:47:47 -0500

    Change helper method to parse any param

--- cmd/api/helper.go
+++ cmd/api/helper.go
@@ -17,22 +17,15 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
-	"strconv"
 )
 
-// readIDParam reads parameters from URL
-func (app *application) readIDParam(r *http.Request) (int64, error) {
+// readParam reads parameters from URL
+func (app *application) readParam(r *http.Request, paramName string) string {
 	params := httprouter.ParamsFromContext(r.Context())
-
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	if err != nil || id < 1 {
-		return 0, errors.New("invalid id parameter")
-	}
-
-	return id, nil
+	data := params.ByName(paramName)
+	return data
 }
 
 // writeJSON helper for sending responses
