e97411334d79afc2e7cf3c1e87e52f783947da48
Pouria Amini <aminipooria1@gmail.com>
2023-02-03 15:15:31 -0500

    Add json readability

--- internal/data/users.go
+++ internal/data/users.go
@@ -6,27 +6,27 @@ import (
 )
 
 type MetaField struct {
-	Key       string
-	Namespace string
-	Value     string
-	Type      string
+	Key       string `json:"key"`
+	Namespace string `json:"namespace"`
+	Value     string `json:"value"`
+	Type      string `json:"type"`
 }
 
 type RangeNumber struct {
-	UpLimit   int64
-	DownLimit int64
+	UpLimit   int64 `json:"up_limit"`
+	DownLimit int64 `json:"down_limit"`
 }
 
 type User struct {
-	ID                     uuid.UUID
-	Email                  string
-	FirstName              string
-	LastName               string
-	ProvinceCode           string
-	CountryCode            string
-	AdministrativeDivision string
-	AgeRange               RangeNumber
-	FamilyNumberRange      RangeNumber
-	CreatedAt              time.Time
-	Meta                   []MetaField
+	ID                     uuid.UUID   `json:"id"`
+	Email                  string      `json:"email"`
+	FirstName              string      `json:"first_name"`
+	LastName               string      `json:"last_name"`
+	ProvinceCode           string      `json:"province_code"`
+	CountryCode            string      `json:"country_code"`
+	AdministrativeDivision string      `json:"administrative_division"`
+	AgeRange               RangeNumber `json:"age_range"`
+	FamilyNumber           int64       `json:"family_number"`
+	CreatedAt              time.Time   `json:"created_at"`
+	Meta                   []MetaField `json:"meta"`
 }
