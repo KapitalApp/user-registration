77c47e3293d706569b7ccf21952b8a74c0042687
Pouria Amini <aminipooria1@gmail.com>
2023-02-04 15:17:32 -0500

    Add configurations for AWS SDK

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-   http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,12 +16,17 @@ limitations under the License.
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	sdkConfig "github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"log"
 	"net/http"
 	"os"
 	"time"
+	"user-service.kptl.net/internal/data"
 )
 
 const version = "1.0.0"
@@ -29,11 +34,16 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	sdk  struct {
+		config aws.Config
+		az     string
+	}
 }
 
 type application struct {
 	config config
 	logger *log.Logger
+	models data.Models
 }
 
 func main() {
@@ -41,13 +51,20 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.sdk.az, "availability-zone", "us-east-1", "AWS Availability Zone")
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
+	err := configSdk(&cfg, logger)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(dynamodb.NewFromConfig(cfg.sdk.config)),
 	}
 
 	srv := &http.Server{
@@ -59,6 +76,23 @@ func main() {
 	}
 
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
+}
+
+func configSdk(cfg *config, logger *log.Logger) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	sdkCfg, err := sdkConfig.LoadDefaultConfig(
+		ctx,
+		sdkConfig.WithRegion(cfg.sdk.az),
+		sdkConfig.WithLogger(logger),
+	)
+	if err != nil {
+		return err
+	}
+
+	cfg.sdk.config = sdkCfg
+	return nil
 }
