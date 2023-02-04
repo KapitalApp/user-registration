41644d6b9a3ab61dec5c380058d30c224cff9765
Pouria Amini <aminipooria1@gmail.com>
2023-02-04 18:48:58 -0500

    Add DynamoDb operations

--- internal/data/users.go
+++ internal/data/users.go
@@ -17,6 +17,14 @@ limitations under the License.
 package data
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/expression"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/google/uuid"
 	"time"
 	"user-service.kptl.net/internal/validator"
@@ -41,6 +49,126 @@ type User struct {
 	Goals                  []Goal         `json:"goals,omitempty"`
 	CreatedAt              time.Time      `json:"created_at,omitempty"`
 	Meta                   []MetaField    `json:"meta,omitempty"`
+}
+
+func (user User) GetKey() map[string]types.AttributeValue {
+	id, err := attributevalue.Marshal(user.ID)
+	if err != nil {
+		panic(err)
+	}
+
+	return map[string]types.AttributeValue{"id": id}
+}
+
+type UserModel struct {
+	DynamoDbClient *dynamodb.Client
+	TableName      string
+}
+
+func (m UserModel) TableExists() (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DynamoDbClient.DescribeTable(
+		ctx, &dynamodb.DescribeTableInput{TableName: aws.String(m.TableName)},
+	)
+
+	if err != nil {
+		var notFoundEx *types.ResourceNotFoundException
+		if errors.As(err, &notFoundEx) {
+			return false, fmt.Errorf("Table %v does not exist.\n", m.TableName)
+		}
+		return false, fmt.Errorf("Couldn't determine existence of table %v. Here's why: %v\n", m.TableName, err)
+	}
+
+	return true, nil
+}
+
+func (m UserModel) Insert(user *User) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	item, err := attributevalue.MarshalMap(user)
+	if err != nil {
+		panic(err)
+	}
+	_, err = m.DynamoDbClient.PutItem(ctx, &dynamodb.PutItemInput{
+		TableName: aws.String(m.TableName), Item: item,
+	})
+	if err != nil {
+		return fmt.Errorf("Couldn't add item to table. Here's why: %v\n", err)
+	}
+
+	return nil
+}
+
+func (m UserModel) Get(id uuid.UUID) (*User, error) {
+	user := User{ID: id}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	response, err := m.DynamoDbClient.GetItem(ctx, &dynamodb.GetItemInput{
+		Key: user.GetKey(), TableName: aws.String(m.TableName),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't get info about %v. Here's why: %v\n", id, err)
+	} else {
+		err = attributevalue.UnmarshalMap(response.Item, &user)
+		if err != nil {
+			return nil, fmt.Errorf("Couldn't unmarshal response. Here's why: %v\n", err)
+		}
+	}
+
+	return &user, nil
+}
+
+func (m UserModel) Update(user *User, newAttributes map[string]interface{}) (map[string]map[string]interface{}, error) {
+	var err error
+	var response *dynamodb.UpdateItemOutput
+	var attributeMap map[string]map[string]interface{}
+
+	var update expression.UpdateBuilder
+	for k, v := range newAttributes {
+		update.Set(expression.Name(k), expression.Value(v))
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	expr, err := expression.NewBuilder().WithUpdate(update).Build()
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't build expression for update. Here's why: %v\n", err)
+	} else {
+		response, err = m.DynamoDbClient.UpdateItem(ctx, &dynamodb.UpdateItemInput{
+			TableName:                 aws.String(m.TableName),
+			Key:                       user.GetKey(),
+			ExpressionAttributeNames:  expr.Names(),
+			ExpressionAttributeValues: expr.Values(),
+			UpdateExpression:          expr.Update(),
+			ReturnValues:              types.ReturnValueUpdatedNew,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("Couldn't update id %v. Here's why: %v\n", user.ID, err)
+		} else {
+			err = attributevalue.UnmarshalMap(response.Attributes, &attributeMap)
+			if err != nil {
+				return nil, fmt.Errorf("Couldn't unmarshall update response. Here's why: %v\n", err)
+			}
+		}
+	}
+
+	return attributeMap, nil
+}
+
+func (m UserModel) Delete(user *User) error {
+	_, err := m.DynamoDbClient.DeleteItem(context.TODO(), &dynamodb.DeleteItemInput{
+		TableName: aws.String(m.TableName), Key: user.GetKey(),
+	})
+	if err != nil {
+		return fmt.Errorf("Couldn't delete %v from the table. Here's why: %v\n", user.ID, err)
+	}
+	return nil
 }
 
 func ValidateUser(v *validator.Validator, user *User) {
