e4fbf82724488a6e43fe9beac218688f0ad5a1ab
Pouria Amini <aminipooria1@gmail.com>
2023-02-06 16:05:29 -0500

    Improve logging by using jsonlog package

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -27,6 +27,7 @@ import (
 	"os"
 	"time"
 	"user-service.kptl.net/internal/data"
+	"user-service.kptl.net/internal/jsonlog"
 )
 
 const version = "1.0.0"
@@ -42,7 +43,7 @@ type config struct {
 
 type application struct {
 	config config
-	logger *log.Logger
+	logger *jsonlog.Logger
 	models data.Models
 }
 
@@ -54,11 +55,11 @@ func main() {
 	flag.StringVar(&cfg.sdk.az, "availability-zone", "us-east-1", "AWS Availability Zone")
 	flag.Parse()
 
-	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
 
 	err := configSdk(&cfg, logger)
 	if err != nil {
-		logger.Fatal(err)
+		logger.PrintFatal(err, nil)
 	}
 
 	app := &application{
@@ -70,17 +71,22 @@ func main() {
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.port),
 		Handler:      app.routes(),
+		ErrorLog:     log.New(logger, "", 0),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
 
-	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
+	logger.PrintInfo("starting server", map[string]string{
+		"addr": srv.Addr,
+		"env":  cfg.env,
+	})
+
 	err = srv.ListenAndServe()
-	logger.Fatal(err)
+	logger.PrintFatal(err, nil)
 }
 
-func configSdk(cfg *config, logger *log.Logger) error {
+func configSdk(cfg *config, logger *jsonlog.Logger) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
@@ -92,7 +98,7 @@ func configSdk(cfg *config, logger *log.Logger) error {
 			func(service, region string) (aws.Endpoint, error) {
 				return aws.Endpoint{URL: "http://localhost:8000"}, nil
 			})),
-		//sdkConfig.WithLogger(logger),
+		sdkConfig.WithLogger(logger),
 	)
 	if err != nil {
 		return err
