9ffbef2b159513ed989c8cb25caeeccd127b15a0
Pouria Amini <aminipooria1@gmail.com>
2023-03-01 09:43:32 -0500

    Add documentations for models

--- internal/data/models.go
+++ internal/data/models.go
@@ -1,4 +1,3 @@
-// Package data /*
 /*
 Copyright 2023 The Kapital Authors
 
@@ -14,24 +13,33 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
+// Package data is an abstraction on top of the data models and
+// functionalities.
 package data
 
 import (
 	"errors"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"user-service.mykapital.io/internal/user"
 )
 
+// Possible errors passed from a model.
 var (
 	ErrRecordNotFound = errors.New("record not found")
 	ErrEditConflict   = errors.New("edit conflict")
 )
 
+// Models represents the internal models for the server.
 type Models struct {
-	Users UserModel
+	Users user.Model
 }
 
+// NewModels creates Models.
+//
+// For the user model, a DynamoDB client is passed.
 func NewModels(client *dynamodb.Client) Models {
 	return Models{
-		Users: UserModel{DynamoDbClient: client, TableName: "User"},
+		Users: user.Model{DynamoDbClient: client, TableName: "User"},
 	}
 }
